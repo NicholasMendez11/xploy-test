6091dd6a6e61cc0e4dfa7ce480f54b71c223343c
Nicholas Mendez <nicholasmeeendez@gmail.com>
2023-11-15 15:17:30 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 export default function middleware(req:NextRequest) {
   let loggedInToken = req.cookies.get("credentials");
   let pathname = req.nextUrl.pathname;
-  const baseUrl = process.env.BASE_URL;
+  const baseUrl = "https://xploy-test-kquo.vercel.app" ; //!IN PRODUCTION CHANGE TO https://xploy.vercel.app INSTEAD OF localhost:3000
 
   if (!loggedInToken && pathname === "/home") {
     return NextResponse.redirect(`${baseUrl}/auth`);
